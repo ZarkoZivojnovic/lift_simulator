959eac574691b21cc036192d7836b56f90316db8
ZarkoZivojnovic <zivojnovic@gmail.com>
2018-09-04 17:02:28 +0200

    animation

--- main.js
+++ main.js
@@ -1,15 +1,60 @@
 import SolutionLib from "./lib/lib.js";
+import Animate from "./lib/animate.js";
 
-const buildingArr = [
-    [],
-    [3],
-    [4],
-    [],
-    [5, 6, 2, 6],
-    [],
-    []
-],
+const modal = document.getElementById("modal"),
+    liftKeyboard = document.getElementById("keyboard"),
+    buildingArr = [
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        []
+    ],
     liftCapacity = 4;
 
-const solutionLib = new SolutionLib(buildingArr, liftCapacity);
-console.log(solutionLib.calculateStops());
+document.getElementById("floorsSpace").addEventListener("click", event => {
+    if (event.target.className.includes("btn")) {
+        const element = event.target.id,
+            fromFloor = element.split("_")[2];
+        console.log(element.split("_")[0]);
+        event.target.parentNode.style.backgroundColor = "#FF4832";
+        keyboard("show");
+        let toFloor = new Promise((resolve) => {
+            liftKeyboard.addEventListener("click", function selectFloor(ev) {
+                if (ev.target !== ev.currentTarget) {
+                    keyboard("hide");
+                    removeEventListener("click", selectFloor);
+                    resolve(ev.target.id.split("_")[1]);
+                }
+            })
+        });
+        toFloor.then(res => {
+            buildingArr[fromFloor].push(parseInt(res));
+            const img = document.createElement("img");
+            img.src = "assets/passenger.png";
+            document.getElementById("passengers"+fromFloor).appendChild(img);
+            console.log(buildingArr);
+        });
+    }
+});
+
+document.getElementById("lift").addEventListener("click", event => {
+    const solutionLib = new SolutionLib(buildingArr, liftCapacity);
+    const animate = new Animate(buildingArr, solutionLib.calculateStops());
+    animate.animate();
+});
+
+function keyboard(showOrHide) {
+    if (showOrHide === "show") {
+        modal.style.display = "block";
+        setTimeout(() => {
+            liftKeyboard.style.top = "100px";
+        }, 200);
+    } else {
+        liftKeyboard.style.top = "-100vh";
+        setTimeout(() => {modal.style.display = "none";}, 200);
+    }
+
+}
